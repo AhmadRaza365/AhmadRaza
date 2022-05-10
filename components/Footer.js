b165a76d49ccd2a6684f4d2f202629b481df32a3
Ahmad Raza <78949609+AhmadRaza365@users.noreply.github.com>
2022-05-10 21:01:38 +0500

    updating Twitter Handle
    updating Twitter username Handle

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <ul className='flex justify-center my-3 '>
             <a href="https://www.facebook.com/AhmadRaza365" target="_blank" rel='noreferrer'> <FaFacebook  className='text-4xl md:text-6xl mx-2 text-white hover:text-light-primary cursor-pointer transition-all duration-300 ease-in'/> </a>
             <a href="https://www.instagram.com/ahmadraza_365" target="_blank" rel='noreferrer'>  <FaInstagram  className='text-4xl md:text-6xl mx-2 text-white hover:text-light-primary cursor-pointer transition-all duration-300 ease-in'/> </a>
-            <a href="https://twitter.com/Ahmad365Raza" target="_blank" rel='noreferrer'> <FaTwitter className='text-4xl md:text-6xl mx-2 text-white hover:text-light-primary cursor-pointer transition-all duration-300 ease-in' /> </a>
+            <a href="https://twitter.com/AhmadRaza365" target="_blank" rel='noreferrer'> <FaTwitter className='text-4xl md:text-6xl mx-2 text-white hover:text-light-primary cursor-pointer transition-all duration-300 ease-in' /> </a>
             <a href="https://www.linkedin.com/in/ahmadraza365/" target="_blank" rel='noreferrer'>  <FaLinkedin  className='text-4xl md:text-6xl mx-2 text-white hover:text-light-primary cursor-pointer transition-all duration-300 ease-in'/> </a>
             <a href="http://github.com/AhmadRaza365" target="_blank" rel='noreferrer'> <FaGithub  className='text-4xl md:text-6xl mx-2 text-white hover:text-light-primary cursor-pointer transition-all duration-300 ease-in'/></a>
          </ul>
